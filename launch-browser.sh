ab86c0e0bbe37839857abcb099fc7d0532a82fec
josh! <github@joshhighet.com>
2025-10-03 16:16:52 +1300

    fix: bidi/cdp was always launching

--- launch-browser.sh
+++ launch-browser.sh
@@ -5,8 +5,15 @@ source /home/toruser/logging.sh
 log_debug "browser" "launching Tor Browser with URL: $BROWSER_URL"
 log_debug "browser" "EXPOSE_REMOTE_DEBUGGER=${EXPOSE_REMOTE_DEBUGGER:-false}"
 
+REMOTE_DEBUG_FLAG=""
+case "${EXPOSE_REMOTE_DEBUGGER:-false}" in
+    [Tt][Rr][Uu][Ee])
+        REMOTE_DEBUG_FLAG="--remote-debugging-port=9223"
+        ;;
+esac
+
 /home/toruser/tor-browser/Browser/start-tor-browser \
---new-window "$BROWSER_URL" ${EXPOSE_REMOTE_DEBUGGER:+ --remote-debugging-port=9223} &
+--new-window "$BROWSER_URL" ${REMOTE_DEBUG_FLAG:+$REMOTE_DEBUG_FLAG} &
 
 for i in {1..6}; do
     sleep 5
